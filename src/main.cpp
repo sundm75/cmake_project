20c819cb3ca12fd1ed870e27903402e1201ffa5b
sundongmei <sundm75@163.com>
2026-06-30 15:21:11 +0800

    add third_party/easyx

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "process.h"
+#include "show_Result.h"
 using std::cout, std::endl;
 
 int main()
@@ -8,6 +9,9 @@ int main()
     Process pro;
     pro.planProcess();
     cout << "planning end" << endl;
+
+    ShowResult show;
+    show.drawResult();
 
     return 0;
 
---
+++ src/show_result/show_result.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "show_result.h"
+#include <graphics.h>
+
+void ShowResult::drawResult()
+{
+    initgraph(1000, 1000);
+    setbkcolor(WHITE);
+    cleardevice();
+
+    std::cout<<"draw circles: "<<std:: endl;
+    setlinecolor(BLACK);
+    setlinestyle(PS_SOLID, 4);
+    circle(500, 500, 400);
+    circle(500, 500, 300);
+
+    system("pause   ");
+}
