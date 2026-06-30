2dcbdd371ac4cee7ec07a56920c70b434966a9b7
sundongmei <sundm75@163.com>
2026-06-30 15:35:02 +0800

    add Eigen3 third_party

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include "process.h"
+#include "Eigen/Dense"
+
+using Eigen::MatrixXd;
+
 using std::cout, std::endl;
 void Process::planProcess()
 {
     cout << "this is planing process" << endl;
     my_map.map_Info();
-    cout << "planning success" << endl;
+    cout << "planning success" << endl; 
+
+   MatrixXd m(2,2);
+    m(0, 0) = 3;
+    m(1, 0) = 5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+
+    cout << m << endl;
 }
